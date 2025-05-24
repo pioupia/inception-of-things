8ebcdeb038ceb7f5d1ea6cdcf3e77f90faf5cabc
pioupia <pioupiadeux@gmail.com>
2025-05-24 17:53:36 +0200

    Feat(Vagrant): Finish the first part of IoT

---
+++ scripts/worker_configuration.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Get the path of shared k3s config
+
+for i in {1..30}; do
+  if [ -f "/vagrant/shared/k3s.yaml" ]; then
+	break
+  fi
+  sleep 2
+done
+
+if [ -f "/vagrant/shared/k3s.yaml" ]; then
+  sudo mkdir -p /home/vagrant/.kube
+  sudo mkdir -p /root/.kube
+
+  sudo sed "s/127.0.0.1/$SERVER_IP/g" /vagrant/shared/k3s.yaml > /home/vagrant/.kube/config
+
+  sudo chmod 400 /home/vagrant/.kube/config
+
+  sudo cp /home/vagrant/.kube/config /root/.kube/config
+
+  sudo chown vagrant /home/vagrant/.kube/config
+  sudo chown root /root/.kube/config
+else
+  echo "Shared k3s configuration file not found. Please ensure the server is running and the file is shared." 1>&2
+  exit 1
+fi
+
+if [ -f "/vagrant/shared/token" ]; then
+  sudo systemctl start k3s
+else
+  echo "Shared k3s token file not found. Please ensure the server is running and the file is shared." 1>&2
+  exit 1
+fi

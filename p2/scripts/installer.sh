42b2e5711d5f569070e29431cf46c1cd6c2d9319
Pioupia <pioupiadeux@gmail.com>
2026-01-02 14:28:36 +0100

    Feat(p2): Finish p2 configuration

--- p2/scripts/installer.sh
+++ p2/scripts/installer.sh
@@ -15,3 +15,12 @@ if [ ! -f /etc/systemd/system/k3s.service.env ]; then
 fi
 
 sudo chmod 600 /etc/systemd/system/k3s.service.env
+
+
+if [ ! -f /usr/local/bin/helm ]; then
+	wget -q 'https://get.helm.sh/helm-v3.9.0-linux-amd64.tar.gz' -O /tmp/helm-v3.9.0-linux-amd64.tar.gz
+	tar -C /tmp -xf /tmp/helm-v3.9.0-linux-amd64.tar.gz
+	mv /tmp/linux-amd64/helm /usr/local/bin/helm
+	rm /tmp/helm-v3.9.0-linux-amd64.tar.gz
+	rm -rf /tmp/linux-amd64
+fi
--- p2/scripts/starter.sh
+++
@@ -1,3 +0,0 @@
-sudo -- bash -c 'echo "SERVER_IP=#{SERVICES['server'][:ip]}" >> /etc/systemd/system/k3s.service.env'
-sudo systemctl start k3s
-sudo systemctl enable k3s

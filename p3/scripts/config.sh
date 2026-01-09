2fc62c3e33546a56a7589ad70607d2f564375d6c
pioupia <49518790+pioupia@users.noreply.github.com>
2026-01-09 15:25:30 +0100

    Feat(Traefik): Try to add ingress to argocd (#8)
    * Feat(Traefik): Try to add ingress to argocd
    
    * Feat(Traefik): Route correctly argocd and deployed playground service

--- p3/scripts/config.sh
+++ p3/scripts/config.sh
@@ -1,6 +1,6 @@
 function create_cluster()
 {
-	k3d cluster create gitops
+	k3d cluster create gitops -p "8443:443@loadbalancer" -p "8080:80@loadbalancer"
 }
 
 function create_app()
@@ -16,10 +16,30 @@ function create_app()
 
 	# Define autosync of playground
 	argocd app set playground --sync-policy automated
+
+	kubectl apply -n dev -f ./confs/playground-expose.yaml
+}
+
+function change_tls()
+{
+	# Install cert-manager
+	kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.19.2/cert-manager.yaml
+	
+	# Wait for cert-manager to be available or wait at most 2min
+	kubectl wait --for=condition=Available deployment --all -n cert-manager --timeout=120s
+
+	kubectl apply -f ./confs/cert-manager.yaml
+
+	kubectl -n argocd get cm argocd-cmd-params-cm -o yaml | sed -e '$a\\ndata:\n  server.insecure: "true"\n' | kubectl replace -n argocd -f -
+
+	kubectl apply -n argocd -f ./confs/argocd-ingress.yaml
+
+	kubectl delete pods -n argocd "$(kubectl get pods | grep argocd-server | awk '{ print $1 }')"
 }
 
 function expose_port()
 {
-	# Expose argocd locally:
-    kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+	change_tls
+
+	echo "127.0.0.1 argocd.gitops.com gitops.com" >> /etc/hosts
 }

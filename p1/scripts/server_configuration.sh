373c1d3875f5522e998aea720b35de8629cebdc2
Pioupia <pioupiadeux@gmail.com>
2026-01-14 20:53:39 +0100

    Fix(P1): Improve values from subject but still issues with VMs IP

--- p1/scripts/server_configuration.sh
+++ p1/scripts/server_configuration.sh
@@ -1,23 +1,7 @@
 #!/bin/sh
 
 export KUBECONFIG_PATH="/home/vagrant/.kube"
-
-# Copy the k3s config for kubectl
 mkdir -p "$KUBECONFIG_PATH"
-sudo mkdir -p /root/.kube
-
-# Share k3s config to other VM
-if [ -f '/etc/rancher/k3s/k3s.yaml' ]; then
-  sudo cp /etc/rancher/k3s/k3s.yaml "${KUBECONFIG_PATH}/config"
-  sudo cp /etc/rancher/k3s/k3s.yaml "/root/.kube/config"
-  sudo chown vagrant "${KUBECONFIG_PATH}/config"
-
-  # Share the kubeconfig with the other vagrant VMs
-  sudo cp /etc/rancher/k3s/k3s.yaml "/vagrant/shared/"
-else
-  echo "K3s configuration file not found. Please ensure K3s is installed and running." 1>&2
-  exit 1
-fi
 
 if [ -f "/var/lib/rancher/k3s/server/token" ]; then
   sudo cp /var/lib/rancher/k3s/server/token "/vagrant/shared/"
--- p1/scripts/worker_configuration.sh
+++ p1/scripts/worker_configuration.sh
@@ -1,41 +1,12 @@
 #!/bin/sh
 
-# Get the path of shared k3s config
-
-for i in {1..30}; do
-  if [ -f "/vagrant/shared/k3s.yaml" ]; then
-	break
+# Waiting at most 5 minutes the server setup it's file properly
+for i in {1..60}; do
+  if [ -f "/vagrant/shared/token" ]; then
+    break
   fi
-  sleep 2
+  sleep 5
 done
 
-if [ -f "/vagrant/shared/k3s.yaml" ]; then
-  sudo mkdir -p /home/vagrant/.kube
-  sudo mkdir -p /root/.kube
-
-  sudo sed "s/127.0.0.1/$SERVER_IP/g" /vagrant/shared/k3s.yaml > /home/vagrant/.kube/config
-
-  sudo rm /vagrant/shared/k3s.yaml
-
-  sudo chmod 400 /home/vagrant/.kube/config
-
-  sudo cp /home/vagrant/.kube/config /root/.kube/config
-
-  sudo chown vagrant /home/vagrant/.kube/config
-  sudo chown root /root/.kube/config
-else
-  echo "Shared k3s configuration file not found. Please ensure the server is running and the file is shared." 1>&2
-  exit 1
-fi
-
-if [ -f "/vagrant/shared/token" ]; then
-  sudo cp /vagrant/shared/token /root/.token
-  sudo chmod 400 /root/.token
-  sudo rm /vagrant/shared/token
-
-  sudo systemctl start k3s
-  sudo systemctl enable k3s
-else
-  echo "Shared k3s token file not found. Please ensure the server is running and the file is shared." 1>&2
-  exit 1
-fi
+# Install k3s 
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent -i ${WORKER_IP}" K3S_URL="https://$SERVER_IP:6443" K3S_TOKEN="$(cat /vagrant/shared/token)" sh -
